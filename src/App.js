fb0b87e0e87941073405b45a644e16adb8a4f83c
nijaskhan <nijaskhan93@gmail.com>
2023-09-30 10:17:51 +0530

    videoCall_app layoutCompleted, sidepanel design finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
+import { Box, Grid, GridItem, Image, useMediaQuery } from '@chakra-ui/react';
+import avatarIcon from './images/avatar.svg';
+import icon from './images/videoSidepanel.svg';
+import icon1 from './images/alerticon.svg';
+import calenderIcon from './images/calender.svg';
+import settingsIcon from './images/settings.svg';
+import videoIcon from './images/videoIcon.svg';
+import icon3 from './images/icon3.svg';
 import './App.css';
 
 function App() {
+
+  const [isLargerThanLg] = useMediaQuery("(min-width: 64em)");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Box textAlign="center" fontSize="xl">
+
+
+        <Grid
+          h='100vh'
+          templateRows='repeat(2, 1fr)'
+          templateColumns='repeat(15, 1fr)'
+          gap={2}
+          padding={3}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {/* sidebar */}
+          <GridItem
+            rowSpan={2}
+            colSpan={1}
+            bg="#f8f8f8"
+            borderRadius={12}
+            border="solid"
+            borderColor="#e0e0e0"
+            paddingY={4}
+            display={isLargerThanLg ? "block" : "none"}
+          >
+            <Grid
+              h='100%'
+              templateRows='repeat(10, 1fr)'
+            >
+              <GridItem
+                rowSpan={1}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Image src={avatarIcon} alt="Your Image" />
+              </GridItem>
+              <GridItem
+                rowSpan={7}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <span style={{ display: 'block' }}>
+                  <Image src={icon} style={{ paddingTop: '24px', width: '50px', height: '50px' }} alt="Your Image" />
+                  <Image src={icon1} style={{ paddingTop: '24px', width: '50px', height: '50px' }} alt="Your Image" />
+
+                  <span style={{ display: 'block', borderRadius: '50%', backgroundColor: '#19A6F4', width: '50px', height: '50px', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '24px' }}>
+                    <Image src={videoIcon} alt="Your Image" />
+                  </span>
+
+
+                  {/* <Image src={avatarIcon} style={{paddingTop: '24px'}} alt="Your Image" /> */}
+
+                  <Image src={icon3} style={{ paddingTop: '24px', width: '50px', height: '50px' }} alt="Your Image" />
+                  <Image src={calenderIcon} style={{ paddingTop: '24px', width: '50px', height: '50px' }} alt="Your Image" />
+                  <Image src={settingsIcon} style={{ paddingTop: '24px', width: '50px', height: '50px' }} alt="Your Image" />
+                </span>
+
+              </GridItem>
+            </Grid>
+          </GridItem>
+
+
+          <GridItem rowSpan={2} colSpan={'10'} bg='papayawhip' borderRadius={12} />
+          <GridItem colSpan={isLargerThanLg ? '4' : '5'} bg='papayawhip' borderRadius={12} />
+          <GridItem colSpan={isLargerThanLg ? '4' : '5'} bg='papayawhip' borderRadius={12} />
+        </Grid>
+
+
+
+
+      </Box>
+    </>
   );
 }
 
